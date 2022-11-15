00df3980941eca189c0830e95191cf1e5c76bc01
Andrew-Dyachenko <north.inhale@gmail.com>
2022-11-15 20:51:01 +0400

    refactor: favigon html prototype removed
    The favicon is already generated and installed. The prototype used to generate the image from the node screenshot is no longer tender. We delete it.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import "./styles/App.scss";
 function App() {
 	return (
 		<div className="App">
-			<div className='logo-wrapper'>
-				<div className='logo'>HB</div>
-			</div>
+			{/* <div className="container">
+				<header className="header">
+					<a href="/" className="brand header__brand">
+						<span className="brand__forehead">
+							HomeBuddy
+							<span className="brand__dot">.</span>
+						</span>
+						<span className="brand__slogan">FOR HOMEOWNERS</span>
+					</a>
+				</header>
+				<nav>
+					<ul>
+						<li>
+							<a href="#form">Link to form</a>
+						</li>
+					</ul>
+				</nav>
+			</div> */}
 		</div>
 	);
 }
