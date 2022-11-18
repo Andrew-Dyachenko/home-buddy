6559021273fb810f7cd24411f701ac9eba2fd916
Andrew-Dyachenko <north.inhale@gmail.com>
2022-11-18 15:34:50 +0400

    fix: missed throne__label block
    I forget to add .throne__label block

--- components/Throne/Throne.js
+++ components/Throne/Throne.js
@@ -23,22 +23,46 @@ export default function Throne() {
 							Ideal for detached and semi-detached homes with sloped roofs.
 						</p>
 					</div>
-					<figure className={styles.throne__figure}>
-						<Image
-							className={styles.throne__image}
-							src='/img/facade.png'
-							alt="Facade"
-							width={581}
-							height={326}
-							objectFit='contain'
-							sizes="(max-width: 375px) 343px, 581px"
-							priority={true}
-						/>
-						<figcaption className='visually-hidden'>
-							The facade, which depicts a part of a brick wall connected to the
-							siding. A coniferous tree grows against the background of the wall
-						</figcaption>
-					</figure>
+					<div className={styles.throne__media}>
+						<figure className={styles.throne__figure}>
+							<Image
+								className={styles.throne__image}
+								src='/img/facade.png'
+								alt="Facade"
+								width={581}
+								height={326}
+								objectFit='contain'
+								sizes="(max-width: 375px) 343px, 581px"
+								priority={true}
+							/>
+							<figcaption className='visually-hidden'>
+								The facade, which depicts a part of a brick wall connected to
+								the siding. A coniferous tree grows against the background of
+								the wall
+							</figcaption>
+						</figure>
+						<a
+							href="https://google.com"
+							rel="noreferrer"
+							target="_blank"
+							className={styles.throne__label}
+						>
+							<h6 className={styles["throne__label-title"]}>As seen on</h6>
+							<figure className={styles["throne__label-figure"]}>
+								<Image
+									className={styles["throne__label-icon"]}
+									src='/img/nbc.png'
+									alt="Logo of the NBC"
+									width={32}
+									height={32}
+									sizes="(max-width: 375px) 23.48px, 32px"
+								/>
+								<figcaption className='visually-hidden'>
+									NBC - National Broadcasting Company
+								</figcaption>
+							</figure>
+						</a>
+					</div>
 				</div>
 			</div>
 		</section>
