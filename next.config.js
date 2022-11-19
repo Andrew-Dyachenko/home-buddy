e21866dadf97139fd1447aa769d374d7b50d2210
Andrew-Dyachenko <north.inhale@gmail.com>
2022-11-19 18:17:03 +0400

    refactor(next.config.js): images quality 75 -> 85

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	},
 	images: {
 		formats: ["image/avif", "image/webp"],
-		quality: 75,
+		quality: 85,
 		deviceSizes: [320, 375, 480, 600, 768, 924, 1202 /*, 1680, 1920*/],
 		minimumCacheTTL: 60 * 60 * 24 * 365,
 	},
