8960deca94dc9a78e7d51d96a2c592a82b01cae8
Andrew-Dyachenko <north.inhale@gmail.com>
2023-12-12 02:54:48 +0400

    feat: yandex metrika added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../src/styles/global.scss";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Head from "next/head";
 import { Montserrat } from "@next/font/google";
 
@@ -10,6 +10,22 @@ const montserrat = Montserrat({
 });
 
 function App({ Component, pageProps }) {
+	useEffect(() => {
+		// <!-- Yandex.Metrika counter -->
+		(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			m[i].l=1*new Date();
+			for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+			k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+			ym(91366449, "init", {
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+			});
+		// <!-- /Yandex.Metrika counter -->
+	 }, []);
 	return (
 		<Fragment>
 			<style jsx={true} global={true}>
